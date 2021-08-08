597a7de09c878da4661a656cfb57344d24037cdf
kvprasoon <prasoonkarunan5@hotmail.com>
2021-08-08 19:24:10 +0530

    update site with keynote spaeaker

---
+++ partners/_index.m
@@ -0,0 +1,20 @@
+---
+title: Sponsors
+menu:
+  main:
+    weight: 20
+
+draft: false
+---
+
+{{% hero %}}
+
+
+{{% /hero %}}
+
+
+<!-- Parteners list -->
+
+{{% partners categories="platinium,gold,silver" %}}
+
+{{% /partners %}}
